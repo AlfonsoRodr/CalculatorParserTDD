b8802431c5a32982be3cb71ed914b9c1a417ca00
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-06-04 17:42:20 +0200

    Co-authored-by: CazaMopis43 <CazaMopis43@users.noreply.github.com>

--- CalculatorParser/src/main/java/es/codeurjc/test/CalculatorParser.java
+++ CalculatorParser/src/main/java/es/codeurjc/test/CalculatorParser.java
@@ -3,7 +3,9 @@ package es.codeurjc.test;
 public class CalculatorParser {
 
     public int parse(String expression) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if ("1".equals(expression)) {
+            return 1;
+        }
+        throw new IllegalArgumentException("Expected number 1");
     }
-	
 }
--- CalculatorParser/src/test/java/es/codeurjc/test/CalculatorParserTest.java
+++ CalculatorParser/src/test/java/es/codeurjc/test/CalculatorParserTest.java
@@ -1,5 +1,21 @@
 package es.codeurjc.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorParserTest {
+    private CalculatorParser calculator;
     
+    @BeforeEach
+    public void setup(){
+        calculator = new CalculatorParser();
+    }
+
+    @Test
+    public void TestOneNumber(){
+        String operation = "1";
+        int res = this.calculator.parse(operation);
+        assertEquals(1,res);
+    }
 }
